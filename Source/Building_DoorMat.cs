713814f4b981d1411ee3262079d67dddbc9558a1
Alias <25497382+Alias44@users.noreply.github.com>
2024-04-28 21:33:11 -0700

    Whitespace changes

--- Source/Building_DoorMat.cs
+++ Source/Building_DoorMat.cs
@@ -1,44 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using UnityEngine;
+﻿using System.Collections.Generic;
 using RimWorld;
 using Verse;
-using Verse.AI;
 using System.Reflection;
 
 namespace SyrDoorMats
 {
-    public class Building_DoorMat : Building
-    {
-        public static FieldInfo carriedFilthList = typeof(Pawn_FilthTracker).GetField("carriedFilth", BindingFlags.NonPublic | BindingFlags.Instance);
+	public class Building_DoorMat : Building
+	{
+		public static FieldInfo carriedFilthList = typeof(Pawn_FilthTracker).GetField("carriedFilth", BindingFlags.NonPublic | BindingFlags.Instance);
 
-        public void Notify_PawnApproaching(Pawn pawn)
-        {
-            if (!pawn.IsPrisonerOfColony && !pawn.IsSlaveOfColony && (pawn.Faction == null || pawn.Faction.HostileTo(Faction.OfPlayer)))
-            {
-                return;
-            }
-            if (pawn.Drafted || pawn.health.hediffSet.BleedRateTotal > 0.01)
-            {
-                return;
-            }
-            if (pawn.CurJob != null && (pawn.CurJobDef == JobDefOf.Flee || pawn.CurJobDef == JobDefOf.FleeAndCower || pawn.CurJobDef == JobDefOf.TendPatient || pawn.CurJobDef.driverClass == typeof(JobDriver_TakeToBed)))
-            {
-                return;
-            }
-            List<Filth> carriedFilth = (List<Filth>)carriedFilthList.GetValue(pawn.filth);
-            if (!carriedFilth.NullOrEmpty())
-            {
-                Filth filth = carriedFilth.RandomElement();
-                FilthMaker.TryMakeFilth(Position, Map, filth.def, filth.sources);
-                carriedFilthList.SetValue(pawn.filth, new List<Filth>());
-            }
-        }
-        public override void DrawGUIOverlay()
-        {
-            return;
-        }
-    }
+		public void Notify_PawnApproaching(Pawn pawn)
+		{
+			if (!pawn.IsPrisonerOfColony && !pawn.IsSlaveOfColony && (pawn.Faction == null || pawn.Faction.HostileTo(Faction.OfPlayer)))
+			{
+				return;
+			}
+			if (pawn.Drafted || pawn.health.hediffSet.BleedRateTotal > 0.01)
+			{
+				return;
+			}
+			if (pawn.CurJob != null && (pawn.CurJobDef == JobDefOf.Flee || pawn.CurJobDef == JobDefOf.FleeAndCower || pawn.CurJobDef == JobDefOf.TendPatient || pawn.CurJobDef.driverClass == typeof(JobDriver_TakeToBed)))
+			{
+				return;
+			}
+			List<Filth> carriedFilth = (List<Filth>)carriedFilthList.GetValue(pawn.filth);
+			if (!carriedFilth.NullOrEmpty())
+			{
+				Filth filth = carriedFilth.RandomElement();
+				FilthMaker.TryMakeFilth(Position, Map, filth.def, filth.sources);
+				carriedFilthList.SetValue(pawn.filth, new List<Filth>());
+			}
+		}
+		public override void DrawGUIOverlay()
+		{
+			return;
+		}
+	}
 }
--- Source/HarmonyPatches.cs
+++ Source/HarmonyPatches.cs
@@ -1,64 +1,59 @@
 ﻿using HarmonyLib;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Reflection;
 using RimWorld;
 using Verse;
-using UnityEngine;
 using Verse.AI;
-using RimWorld.Planet;
-using System.Reflection.Emit;
+
 
 namespace SyrDoorMats
 {
-    [StaticConstructorOnStartup]
-    public static class HarmonyPatches
-    {
-        static HarmonyPatches()
-        {
-            var harmony = new Harmony("Syrchalis.Rimworld.Doormats");
-            harmony.PatchAll(Assembly.GetExecutingAssembly());
-        }
-    }
+	[StaticConstructorOnStartup]
+	public static class HarmonyPatches
+	{
+		static HarmonyPatches()
+		{
+			var harmony = new Harmony("Syrchalis.Rimworld.Doormats");
+			harmony.PatchAll(Assembly.GetExecutingAssembly());
+		}
+	}
 
-    [HarmonyPatch(typeof(Pawn_PathFollower), "SetupMoveIntoNextCell")]
-    public static class SetupMoveIntoNextCellPatch
-    {
-        [HarmonyPostfix]
-        public static void SetupMoveIntoNextCell_Postfix(Pawn_PathFollower __instance, Pawn ___pawn)
-        {
-            if (___pawn != null)
-            {
-                Building_DoorMat building_DoorMat = ___pawn.Map.thingGrid.ThingAt<Building_DoorMat>(__instance.nextCell);
-                building_DoorMat?.Notify_PawnApproaching(___pawn);
-            }
-        }
-    }
+	[HarmonyPatch(typeof(Pawn_PathFollower), "SetupMoveIntoNextCell")]
+	public static class SetupMoveIntoNextCellPatch
+	{
+		[HarmonyPostfix]
+		public static void SetupMoveIntoNextCell_Postfix(Pawn_PathFollower __instance, Pawn ___pawn)
+		{
+			if (___pawn != null)
+			{
+				Building_DoorMat building_DoorMat = ___pawn.Map.thingGrid.ThingAt<Building_DoorMat>(__instance.nextCell);
+				building_DoorMat?.Notify_PawnApproaching(___pawn);
+			}
+		}
+	}
 
-    [HarmonyPatch(typeof(Pawn_PathFollower), "CostToMoveIntoCell", new Type[] { typeof(Pawn), typeof(IntVec3) })]
-    public static class CostToMoveIntoCellPatch
-    {
-        [HarmonyPostfix]
-        public static void CostToMoveIntoCell_Postfix(ref float __result, Pawn_PathFollower __instance, Pawn pawn, IntVec3 c)
-        {
-            if (DoorMatsSettings.slowdown > 0 && pawn != null && (pawn.IsColonist || pawn.IsPrisonerOfColony || pawn.IsSlaveOfColony))
-            {
-                if (pawn.Drafted || pawn.health.hediffSet.BleedRateTotal > 0.01)
-                {
-                    return;
-                }
-                if (pawn.CurJob != null && (pawn.CurJobDef == JobDefOf.Flee || pawn.CurJobDef == JobDefOf.FleeAndCower || pawn.CurJobDef == JobDefOf.TendPatient || pawn.CurJobDef.driverClass == typeof(JobDriver_TakeToBed)))
-                {
-                    return;
-                }
-                Building_DoorMat building_DoorMat = pawn.Map.thingGrid.ThingAt<Building_DoorMat>(c);
-                if (building_DoorMat != null)
-                {
-                    __result += DoorMatsSettings.slowdown;
-                }
-            }
-        }
-    }
+	[HarmonyPatch(typeof(Pawn_PathFollower), "CostToMoveIntoCell", new Type[] { typeof(Pawn), typeof(IntVec3) })]
+	public static class CostToMoveIntoCellPatch
+	{
+		[HarmonyPostfix]
+		public static void CostToMoveIntoCell_Postfix(ref float __result, Pawn_PathFollower __instance, Pawn pawn, IntVec3 c)
+		{
+			if (DoorMatsSettings.slowdown > 0 && pawn != null && (pawn.IsColonist || pawn.IsPrisonerOfColony || pawn.IsSlaveOfColony))
+			{
+				if (pawn.Drafted || pawn.health.hediffSet.BleedRateTotal > 0.01)
+				{
+					return;
+				}
+				if (pawn.CurJob != null && (pawn.CurJobDef == JobDefOf.Flee || pawn.CurJobDef == JobDefOf.FleeAndCower || pawn.CurJobDef == JobDefOf.TendPatient || pawn.CurJobDef.driverClass == typeof(JobDriver_TakeToBed)))
+				{
+					return;
+				}
+				Building_DoorMat building_DoorMat = pawn.Map.thingGrid.ThingAt<Building_DoorMat>(c);
+				if (building_DoorMat != null)
+				{
+					__result += DoorMatsSettings.slowdown;
+				}
+			}
+		}
+	}
 }
